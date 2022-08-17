1e310ad9253ac71c645260be4cf0527472f34cc9
DESKTOP-C5SN999\Mykola <mykola.matviienko7@gmail.com>
2022-08-17 23:13:13 +0300

    data from database has been received

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const app = express();
 const router = express.Router();
 const PORT = 3000;
 const mysql = require("mysql2");
+const myDatabaseAddressObject = {
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  database: "myfuelmapdb",
+  password: "admin",
+};
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -18,17 +25,23 @@ app.listen(PORT, () => {
   console.log(`Server running at: http://localhost:${PORT}/`);
 });
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: "3306",
-  user: "root",
-  database: "myfuelmapdb",
-  password: "admin",
-});
-connection.connect(function (err) {
-  if (err) {
-    return console.error("Ошибка: " + err.message);
-  } else {
-    console.log("Подключение к серверу MySQL успешно установлено");
-  }
-});
+let temp = null;
+getDataFromDatabase(myDatabaseAddressObject, temp);
+
+function getDataFromDatabase(connectionParametersObject, dataFromDB) {
+  const connection = mysql.createConnection(connectionParametersObject);
+  connection.connect(function (err) {
+    if (err) {
+      return console.error("Ошибка: " + err.message);
+    } else {
+      console.log("Подключение к серверу MySQL успешно установлено");
+    }
+  });
+  connection.query("SELECT * FROM refueling_list", (err, rows) => {
+    if (err) throw err;
+
+    dataFromDB = rows;
+    console.log("Data received from Db:");
+    console.log(rows);
+  });
+}
